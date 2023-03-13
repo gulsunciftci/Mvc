80f23138e27b2997d3ae4c9aab77ae59a35a46d2
Gulsun Ciftci <ciftcigulsun8@gmail.com>
2023-03-13 03:09:03 +0300

    Yeni Ürün Ekleme Sayfa Tasarımı ve müşteri eklemeler yapıldı

--- MvcStok/Controllers/UrunController.cs
+++ MvcStok/Controllers/UrunController.cs
@@ -16,5 +16,17 @@ namespace MvcStok.Controllers
             return View(degerler);
          
         }
+        [HttpGet]
+        public ActionResult YeniUrun()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult YeniUrun(TBLURUNLER p1)
+        {
+            db.TBLURUNLER.Add(p1);
+            db.SaveChanges();
+            return View();
+        }
     }
 }
